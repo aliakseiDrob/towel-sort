2b786b625d384799ee1c282899e2770b6264cba5
Aleksei Drob <aliakseidrob@gmail.com>
2023-08-07 14:17:02 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,50 @@
+"use strict";
 
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
-}
+    const isArgumentsListEmpty = () => {
+      if ([...arguments].length === 0) {
+          return true;
+      }
+    }
+
+    if (isArgumentsListEmpty()) {
+        return [];
+      }
+
+    const matrixCopy = matrix.map(subArray => subArray.slice());
+    
+    const isMatrixEmpty = (matrixCopy) => {
+        if (matrixCopy.length === 0) {
+            return true;
+        }
+    }
+
+    if (isMatrixEmpty(matrixCopy)) {
+        return matrixCopy;
+      }
+    
+    const reverseOddRows = (element, index, matrixCopy) => {
+        if (index % 2 !== 0) {
+          return matrixCopy[index].reverse();
+        }
+      }
+  
+      const convertMatrixToSimpleArray = (matrixCopy, simpleArray) => {
+        for (let i = 0; i < matrixCopy.length; i++) {
+            for (let j = 0; j < matrixCopy[i].length; j++) {
+                simpleArray.push(matrixCopy[i][j])
+            }
+        }
+      }
+  
+      matrixCopy.map(reverseOddRows);
+  
+      const simpleArray = [];
+      
+      convertMatrixToSimpleArray(matrixCopy, simpleArray);
+  
+      return simpleArray;
+  }
+
