fee938758a4ec48216bc567d04b933b4fbd3ca3e
aliakseidrob <aliakseidrob@gmail.com>
2024-10-20 23:07:10 +0300

    refactor: delete unnecessary conditions

--- src/index.js
+++ src/index.js
@@ -1,50 +1,31 @@
 "use strict";
 
-// You should implement your task here.
-
-module.exports = function towelSort (matrix) {
-    const isArgumentsListEmpty = () => {
-      if ([...arguments].length === 0) {
-          return true;
-      }
-    }
-
-    if (isArgumentsListEmpty()) {
+module.exports = function towelSort(matrix) {
+    if (arguments.length === 0) {
         return [];
-      }
-
-    const matrixCopy = matrix.map(subArray => subArray.slice());
-    
-    const isMatrixEmpty = (matrixCopy) => {
-        if (matrixCopy.length === 0) {
-            return true;
-        }
     }
 
-    if (isMatrixEmpty(matrixCopy)) {
-        return matrixCopy;
-      }
-    
+    const matrixCopy = matrix.map((subArray) => subArray.slice());
+
     const reverseOddRows = (element, index, matrixCopy) => {
         if (index % 2 !== 0) {
-          return matrixCopy[index].reverse();
+            return matrixCopy[index].reverse();
         }
-      }
-  
-      const convertMatrixToSimpleArray = (matrixCopy, simpleArray) => {
+    };
+
+    const convertMatrixToSimpleArray = (matrixCopy, simpleArray) => {
         for (let i = 0; i < matrixCopy.length; i++) {
             for (let j = 0; j < matrixCopy[i].length; j++) {
-                simpleArray.push(matrixCopy[i][j])
+                simpleArray.push(matrixCopy[i][j]);
             }
         }
-      }
-  
-      matrixCopy.map(reverseOddRows);
-  
-      const simpleArray = [];
-      
-      convertMatrixToSimpleArray(matrixCopy, simpleArray);
-  
-      return simpleArray;
-  }
+    };
 
+    matrixCopy.map(reverseOddRows);
+
+    const simpleArray = [];
+
+    convertMatrixToSimpleArray(matrixCopy, simpleArray);
+
+    return simpleArray;
+};
