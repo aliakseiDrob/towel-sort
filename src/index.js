97cb79e3aff1b6361d225a856456b39e918351ec
aliakseidrob <aliakseidrob@gmail.com>
2024-10-21 13:50:11 +0300

    refactor: add ternary operator

--- src/index.js
+++ src/index.js
@@ -4,28 +4,7 @@ module.exports = function towelSort(matrix) {
     if (arguments.length === 0) {
         return [];
     }
-
-    const matrixCopy = matrix.map((subArray) => subArray.slice());
-
-    const reverseOddRows = (element, index, matrixCopy) => {
-        if (index % 2 !== 0) {
-            return matrixCopy[index].reverse();
-        }
-    };
-
-    const convertMatrixToSimpleArray = (matrixCopy, simpleArray) => {
-        for (let i = 0; i < matrixCopy.length; i++) {
-            for (let j = 0; j < matrixCopy[i].length; j++) {
-                simpleArray.push(matrixCopy[i][j]);
-            }
-        }
-    };
-
-    matrixCopy.map(reverseOddRows);
-
-    const simpleArray = [];
-
-    convertMatrixToSimpleArray(matrixCopy, simpleArray);
-
-    return simpleArray;
+    return matrix.reduce((acc, current, index) => {
+        return index % 2 ? acc.concat(current.reverse()) : acc.concat(current);
+    }, []);
 };
---
+++ src/test.js
@@ -0,0 +1,14 @@
+function towelSort(matrix) {
+    return matrix.reduce((acc, current, index) => {
+       
+    }, []);
+}
+
+console.log(
+    towelSort([
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+        [10, 12, 11],
+    ])
+);
