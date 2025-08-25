faf00ac8cf7de4c4463c89ad15e243e9a4bbb965
aliakseidrob <aliakseidrob@gmail.com>
2025-08-25 20:07:32 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function towelSort(/* matrix */) {
-  throw new Error('Not implemented');
+module.exports = function towelSort(matrix) {
+  if (arguments.length === 0) {
+    return [];
+  }
+  return matrix.reduce((acc, current, index) => {
+    return index % 2 ? acc.concat(current.reverse()) : acc.concat(current);
+  }, []);
 };
